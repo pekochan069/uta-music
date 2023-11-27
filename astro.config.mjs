83aa1a2f1d63c7232d4f91e62e000e7864b32641
pekochan069 <pekochan069@gmail.com>
2023-11-28 01:29:04 +0900

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import solidJs from "@astrojs/solid-js";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), solidJs()],
+  output: "server",
+});
---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+import preline from 'preline/plugin.js'
+
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      screens: {
+        "1080p": "1920px",
+        "1440p": "2560px",
+      },
+    },
+  },
+  plugins: [preline],
+  darkMode: "class",
+};
