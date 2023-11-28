5100c83d619b063c420c2a9ce8a23c084d446e2a
pekochan069 <pekochan069@gmail.com>
2023-11-29 00:23:52 +0900

    Add vercel adapter for Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: vercel(),
   integrations: [tailwind(), solidJs()],
   output: "server",
 });
